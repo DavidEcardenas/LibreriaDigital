b49698f4dfdcd0f680c98a8e7c22b33065ba1ec6
David Esteban <agudelodavid88@gmail.com>
2026-04-17 17:55:31 -0500

    mi primer commit

---
+++ LibreriaApp/Libreria.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libreria.WebApi.controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHelloWorld()
+        {
+            return Ok("hola");
+        }
+    }
+}
---
+++ LibreriaApp/Libreria.WebApi/Controllers/librosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Libreria.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class librosController : ControllerBase
+    {
+        // GET: api/<librosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<librosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<librosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<librosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<librosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
